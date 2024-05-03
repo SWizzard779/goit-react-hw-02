7f7519fb6740420e8ae28323824c01072729fc7a
SOFIA <sofia.nesimko@gmail.com>
2024-05-03 09:47:56 +0200

    project structure

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import Description from "../Description/Description"
+import Options from "../Options/Options"
+import Feedback from "../Feedback/Feedback"
+
+
+function App() {
+
+  return (
+    <>
+    <Description/>
+    <Options/>
+    <Feedback/>
+    </>
+  )
+}
+
+export default App
